279b987dcbc0a564fe87acca93df40d42724e7c7
Filriber <filriber120412@outlook.com>
2023-05-18 21:18:09 -0300

    Primeira Tentativa ligacao com banco de dados

--- src/main/java/Classes/Cliente.java
+++ src/main/java/Classes/Cliente.java
@@ -1,10 +1,52 @@
 package Classes;
 
+import java.util.ArrayList;
+
 /**
  * Classe com todas as informacoes do cliente
  * 
  */
 public class Cliente {
+        private String nome;
+        private String endereco;
+        private String cPF;
+        private String cEP;
+        private String email;
+        private int numEndereco;
+        private boolean sexo;
+        private String complemento;
+        private int idCliente;
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+        
+        private ArrayList<Cliente> listaItens = null;
+       
+        public Cliente(){
+            
+        }
+        
+        public Cliente(String nome, String endereco, String cPF, String cEP, int numEndereco, Boolean sexo, String complemento){
+         this.nome = nome;
+         this.endereco = endereco;
+         this.cPF = cPF;
+         this.cEP = cEP;
+         this.numEndereco = numEndereco;
+         this.sexo = sexo;
+         this.complemento = complemento;
+        }
+        public ArrayList <Cliente> getListaItens(){
+            return listaItens;
+            
+        }
+        public void setListaItens (ArrayList<Cliente> listaItens){
+            
+        }
     public String getNome() {
             return nome;
         }
@@ -53,7 +95,7 @@ public class Cliente {
             this.numEndereco = numEndereco;
         }
 
-        public boolean isSexo() {
+        public boolean getSexo() {
             return sexo;
         }
 
@@ -68,12 +110,7 @@ public class Cliente {
         public void setComplemento(String complemento) {
             this.complemento = complemento;
         }
-        private String nome;
-        private String endereco;
-        private String cPF;
-        private String cEP;
-        private String email;
-        private int numEndereco;
-        private boolean sexo;
-        private String complemento;
+  
+        
+        
 }
--- src/main/java/DAO/ClienteDAO.java
+++ src/main/java/DAO/ClienteDAO.java
@@ -4,14 +4,74 @@
  */
 package DAO;
 
+import Classes.Cliente;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  *
  * @author SALOMAO.FLIMA
  */
 public class ClienteDAO {
     
-    static String URL = "jdbc:mysql://localhost:3306/AQUI É O LINK DO BANCO DE DADOS";
+    static String URL = "jdbc:mysql://localhost:3306/CasaDeCarne";
     static String Login = "root";
-    static String Senha = "";
+    static String Senha = "A@1090073061a";
+    
+        public static boolean salvar(Cliente obj) {
+        boolean retorno = false;
+        
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conexao = DriverManager.getConnection(URL, Login, Senha);
+            
+            PreparedStatement comandoSQL = conexao.prepareStatement("INSERT INTO Cliente (Nome,Endereco,CPF,CEP,Email,Numero,Sexo) VALUES (?,?,?,?,?,?,?)", Statement.RETURN_GENERATED_KEYS);
+            comandoSQL.setInt(1, obj.getIdCliente());
+            comandoSQL.setString(1, obj.getNome());
+            comandoSQL.setString(2, obj.getEndereco());
+            comandoSQL.setString(3, obj.getcPF());
+            comandoSQL.setString(4, obj.getcEP());
+            comandoSQL.setString(5, obj.getEmail());
+            comandoSQL.setInt(6, obj.getNumEndereco());
+            comandoSQL.setBoolean(7, obj.getSexo());
+             
+            int linhasAfetadas = comandoSQL.executeUpdate();
+            if (linhasAfetadas > 0) {
+                ResultSet rs = comandoSQL.getGeneratedKeys();
+                if (rs .next()) {
+                    int idCliente = rs.getInt(1);
+                    for (Cliente item : obj.getListaItens()) {
+                        PreparedStatement comandoSQLItem = conexao.prepareStatement("INSERT INTO ItemNota (Nome,Endereco,CPF,CEP,Email,Numero,Sexo) VALUES (?,?,?,?,?,?,?)");
+                        comandoSQLItem.setInt(1, item.getIdCliente());
+                        comandoSQLItem.setString(2, item.getNome());
+                        comandoSQLItem.setString(3, item.getEndereco());
+                        comandoSQLItem.setString(4, item.getcPF());
+                        comandoSQLItem.setString(5,item.getcEP());
+                        comandoSQLItem.setString(6,item.getEmail());
+                        comandoSQLItem.setInt(7,item.getNumEndereco());
+                        comandoSQLItem.setBoolean(8,item.getSexo());
+                        
+
+                        int linhasAfetadasItem = comandoSQLItem.executeUpdate();
+                        if(linhasAfetadasItem>0){
+                            retorno = true;
+                        }
+                    }
+                }
+            }
+            
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        
+        return retorno;
+        
+    }
     
 }
+    
+
