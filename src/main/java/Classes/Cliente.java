f1b69e45f0717b22fe4bc87b15a4552cd58d1e73
Filriber <filriber120412@outlook.com>
2023-05-29 15:25:53 -0300

    foi

--- src/main/java/Classes/Cliente.java
+++ src/main/java/Classes/Cliente.java
@@ -2,52 +2,53 @@ package Classes;
 
 import java.util.ArrayList;
 
-/**
- * Classe com todas as informacoes do cliente
- * 
+/*
+Classes com informacoes do cliente
  */
 public class Cliente {
-        private String nome;
-        private String endereco;
-        private String cpf;
-        private String cep;
-        private String email;
-        private String numEndereco;
-        private String sexo;
-        private String complemento;
-        private int idCliente;
 
-    public int getIdCliente() {
-        return idCliente;
+    private String nome;
+    private String endereco;
+    private String cpf;
+    private String cep;
+    private String email;
+    private String numEndereco;
+    private String sexo;
+    private String complemento;
+    private int idCliente;
+
+    private ArrayList<Cliente> listaItens = null;
+
+    public Cliente() {
+
     }
 
-    public void setIdCliente(int idCliente) {
-        this.idCliente = idCliente;
+    /*
+        construtor da classe
+     */
+    public Cliente(String nome, String endereco, String cpf, String cep, String email, String numEndereco, String sexo, String complemento) {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.cpf = cpf;
+        this.cep = cep;
+        this.email = email;
+        this.numEndereco = numEndereco;
+        this.sexo = sexo;
+        this.complemento = complemento;
     }
-        
-        private ArrayList<Cliente> listaItens = null;
-       
-        public Cliente(){
-            
-        }
-        
-        public Cliente(String nome, String endereco, String cpf, String cep,String email, String numEndereco, String sexo, String complemento){
-         this.nome = nome;
-         this.endereco = endereco;
-         this.cpf = cpf;
-         this.cep = cep;
-         this.email = email;
-         this.numEndereco = numEndereco;
-         this.sexo = sexo;
-         this.complemento = complemento;
-        }
-        public ArrayList <Cliente> getListaItens(){
-            return listaItens;
-            
-        }
-        public void setListaItens (ArrayList<Cliente> listaItens){
-            
-        }
+
+    /*
+        ArrayList para popular as informacoes do Cliente
+     */
+    public ArrayList<Cliente> getListaItens() {
+        return listaItens;
+
+    }
+
+    public void setListaItens(ArrayList<Cliente> listaItens) {
+
+    }
+
     public String getNome() {
         return nome;
     }
@@ -56,62 +57,68 @@ public class Cliente {
         this.nome = nome;
     }
 
-        public String getEndereco() {
-            return endereco;
-        }
+    public String getEndereco() {
+        return endereco;
+    }
 
-        public void setEndereco(String endereco) {
-            this.endereco = endereco;
-        }
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
 
-        public String getCpf() {
-            return cpf;
-        }
+    public String getCpf() {
+        return cpf;
+    }
 
-        public void setCpf(String cpf) {
-            this.cpf = cpf;
-        }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
 
-        public String getCep() {
-            return cep;
-        }
+    public String getCep() {
+        return cep;
+    }
 
-        public void setCep(String cep) {
-            this.cep = cep;
-        }
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
 
-        public String getEmail() {
-            return email;
-        }
+    public String getEmail() {
+        return email;
+    }
 
-        public void setEmail(String email) {
-            this.email = email;
-        }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-        public String getNumEndereco() {
-            return numEndereco;
-        }
+    public String getNumEndereco() {
+        return numEndereco;
+    }
 
-        public void setNumEndereco(String numEndereco) {
-            this.numEndereco = numEndereco;
-        }
+    public void setNumEndereco(String numEndereco) {
+        this.numEndereco = numEndereco;
+    }
 
-        public String getSexo() {
-            return sexo;
-        }
+    public String getSexo() {
+        return sexo;
+    }
 
-        public void setSexo(String sexo) {
-            this.sexo = sexo;
-        }
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
 
-        public String getComplemento() {
-            return complemento;
-        }
+    public String getComplemento() {
+        return complemento;
+    }
 
-        public void setComplemento(String complemento) {
-            this.complemento = complemento;
-        }
-                    
-        
-        
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
 }
